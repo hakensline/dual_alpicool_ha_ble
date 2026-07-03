299fb1060b99b2836a8220fbfbf0f9a966bb811c
hakensline <hakensline@gmail.com>
2026-07-03 07:42:46 +0200

    hh

--- custom_components/alpicool_ble/const.py
+++ custom_components/alpicool_ble/const.py
@@ -5,10 +5,22 @@ DOMAIN = "alpicool_ble"
 CONF_MAC = "mac"
 CONF_NAME = "name"
 
-# Les identifiants de zone indispensables pour climate.py
+# Les identifiants de zone pour le Dual Zone
 ZONE_LEFT = "left"
 ZONE_RIGHT = "right"
 
-# UUID Bluetooth standards utilisés par Alpicool / Outwell
-ALPICOOL_SERVICE_UUID = "00001234-0000-1000-8000-00805f9b34fb"  # À ajuster si ton modèle utilise un autre UUID
+# UUID Bluetooth (Pour notre Coordinateur)
+ALPICOOL_SERVICE_UUID = "00001234-0000-1000-8000-00805f9b34fb"
 ALPICOOL_CHARACTERISTIC_UUID = "0000FFF3-0000-1000-8000-00805f9b34fb"
+
+# UUID Bluetooth (Pour la compatibilité avec le api.py de Gruni22)
+FRIDGE_NOTIFY_UUID = "0000FFF1-0000-1000-8000-00805f9b34fb"
+FRIDGE_RW_CHARACTERISTIC_UUID = "0000FFF3-0000-1000-8000-00805f9b34fb"
+
+class Request:
+    """Commandes pour l'API Alpicool de Gruni22."""
+    BIND = 0x00
+    QUERY = 0x01
+    SET = 0x02
+    SET_LEFT = 0x03
+    SET_RIGHT = 0x04
