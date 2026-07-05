b3013e23319dc76c6cbf20294f3c65ba7ddcb495
hakensline <hakensline@gmail.com>
2026-07-05 17:26:20 +0200

    Update const.py

--- custom_components/alpicool_ble/const.py
+++ custom_components/alpicool_ble/const.py
@@ -5,9 +5,10 @@ DOMAIN = "alpicool_ble"
 CONF_MAC = "mac"
 CONF_NAME = "name"
 
-ZONE_LEFT = "left"
-ZONE_RIGHT = "right"
+# Définition des zones inversées (Gaucher est physiquement la zone droite)
+ZONE_LEFT = "right"   # Home Assistant Gauche = Compartiment Physique Droite (Négatif)
+ZONE_RIGHT = "left"   # Home Assistant Droite = Compartiment Physique Gauche (Positif)
 
-# UUID de dialogue (Écriture de commandes = FFF3 | Écoute des trames = FFF1)
+# UUID de dialogue (Écriture = FFF3 | Écoute = FFF1)
 ALPICOOL_CHARACTERISTIC_UUID = "0000FFF3-0000-1000-8000-00805f9b34fb"
 FRIDGE_NOTIFY_UUID = "0000FFF1-0000-1000-8000-00805f9b34fb"
